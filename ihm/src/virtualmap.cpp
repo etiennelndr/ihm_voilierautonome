c3f6bf0605ba21309ad885832063ddb867ad8143
Sylvain MILBEO <37792045+SylvainM29@users.noreply.github.com>
2018-11-28 18:15:48 +0100

    merge

--- ihm/src/virtualmap.cpp
+++ ihm/src/virtualmap.cpp
@@ -43,6 +43,7 @@ void VirtualMap::display_boats(vector<Boat*> boats,QMainWindow* mw){
 
     QPen pen1(Qt::black);
     QPen pen2(Qt::red);
+    QPen pen3(Qt::red);
 
     pen1.setWidth(8);
     ellipsePainter.setPen(pen1);
@@ -51,50 +52,45 @@ void VirtualMap::display_boats(vector<Boat*> boats,QMainWindow* mw){
     for (unsigned int i = 0; i < boats.size(); i++) {
 
         Boat* boat = boats.at(i);
-        if (boat->get_latitude()>0.0f && boat->get_longitude()>0.0f){
-            ellipsePainter.fillRect(scale_lat(boat->get_latitude()),scale_lon(boat->get_longitude()),20,75,boat->get_color());
+
+        qDebug() << "id : "<< boat->get_id() << " / longitude : " << boat->get_longitude() << " / latitude :" << boat->get_latitude();
+        if (start_latitude<boat->get_latitude()<end_latitude && start_longitude<boat->get_longitude()<end_longitude){
+            ellipsePainter.fillRect(scale_lat(boat->get_latitude()),scale_lon(boat->get_longitude()),10,30,boat->get_color());
             qDebug() <<boat->get_latitude();
         }
         else {
-             ellipsePainter.fillRect(boats.size()*(245-(30/2))+30*i,423,20,75,boat->get_color());
+             ellipsePainter.fillRect(boats.size()*(270-(10/2))+30*i,460,10,30,boat->get_color());
         }
         ellipsePainter.fillRect(245-(boats.size()*30/2)+30*i,720,10,10,boat->get_color());
         ellipsePainter.drawText(245-(boats.size()*30/2)+30*i,745,QString::number(boat->get_id()));
     }
 
-//    ellipsePainter.fillRect(114,292,20,75,Qt::green);
-//    ellipsePainter.drawEllipse(QRect(100,280,50,100));
+    pen2.setWidth(8);
 
-//    ellipsePainter.fillRect(300,450,20,75,Qt::blue);
-//    ellipsePainter.drawEllipse(QRect(286,436,50,100));
     //Affichage des balises
-
     ellipsePainter.setPen(pen2);
-    ellipsePainter.drawEllipse(QRect(scale_lat(balises.at(0)->get_latitude())-5,scale_lon(balises.at(0)->get_longitude())-5,10,10));
-    ellipsePainter.drawEllipse(QRect(scale_lat(balises.at(1)->get_latitude())-5,scale_lon(balises.at(1)->get_longitude())-5,10,10));
-    ellipsePainter.drawEllipse(QRect(scale_lat(balises.at(2)->get_latitude())-5,scale_lon(balises.at(2)->get_longitude())-5,10,10));
-    ellipsePainter.drawEllipse(QRect(scale_lat(balises.at(3)->get_latitude())-5,scale_lon(balises.at(3)->get_longitude())-5,10,10));
+    ellipsePainter.drawEllipse(QRect(scale_lat(balises.at(0)->get_latitude())-10,scale_lon(balises.at(0)->get_longitude())-10,20,20));
+    ellipsePainter.drawEllipse(QRect(scale_lat(balises.at(1)->get_latitude())-10,scale_lon(balises.at(1)->get_longitude())-10,20,20));
+    ellipsePainter.drawEllipse(QRect(scale_lat(balises.at(2)->get_latitude())-10,scale_lon(balises.at(2)->get_longitude())-10,20,20));
+    ellipsePainter.drawEllipse(QRect(scale_lat(balises.at(3)->get_latitude())-10,scale_lon(balises.at(3)->get_longitude())-10,20,20));
 
 
     QRectF rectangle(60, 210, 500, 500);
 
     QPainter painter(mw);
     painter.drawRect(rectangle);
-}
-//int VirtualMap::get_xy(float lat,float lng)
-//{
-//int mapWidth=2058;
-//int mapHeight=1746;
-//float factor=.404;
-//int x_adj=-391;
-//int y_adj=37;
-//float x = (mapWidth*(180+lng)/360)%mapWidth+(mapWidth/2);
-//float latRad = lat*Math.PI/180;
-//float mercN = Math.log(Math.tan((Math.PI/4)+(latRad/2)));
-//float y = (mapHeight/2)-(mapWidth*mercN/(2*Math.PI));
-//return { x: x*factor+x_adj,y: y*factor+y_adj}
-//}
 
+    //Vitesse de vente Vent
+    ellipsePainter.setPen(pen3);
+    pen3.setWidth(16);
+    ellipsePainter.drawEllipse(QRect(670,90,75,75));
+    ellipsePainter.drawText(700,80,"N");
+    ellipsePainter.drawText(700,190,"S");
+    ellipsePainter.drawText(650,135,"W");
+    ellipsePainter.drawText(755,135,"E");
+
+
+}
 int VirtualMap::scale_lat(float real_lat){
     float scaled_lat = (real_lat-start_latitude)*(500/(max(end_latitude-start_latitude,end_longitude-start_longitude)));
     return int(scaled_lat)+60; // offset de l'affichage du carre
